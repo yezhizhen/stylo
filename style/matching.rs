0f024de24d3436fa01c3332ed7d41addfaa5cc9f
Euclid Ye <yezhizhenjiakang@gmail.com>
2025-03-14 15:55:04 +0800

    1. No longer mark Finished transition as CascadeLevel::Transitions 2. Implement step 4.1, 4.2 of CSS transition 3. Remove unnecessary `mut` for readability
    Signed-off-by: Euclid Ye <yezhizhenjiakang@gmail.com>

--- style/matching.rs
+++ style/matching.rs
@@ -644,7 +644,7 @@ trait PrivateMatchMethods: TElement {
     fn process_animations_for_pseudo(
         &self,
         context: &mut StyleContext<Self>,
-        old_styles: &mut ElementStyles,
+        old_styles: &ElementStyles,
         new_resolved_styles: &mut ResolvedElementStyles,
         pseudo_element: PseudoElement,
     ) {
@@ -726,8 +726,8 @@ trait PrivateMatchMethods: TElement {
     fn process_animations_for_style(
         &self,
         context: &mut StyleContext<Self>,
-        old_values: &mut Option<Arc<ComputedValues>>,
-        new_values: &mut Arc<ComputedValues>,
+        old_values: &Option<Arc<ComputedValues>>,
+        new_values: &Arc<ComputedValues>,
         pseudo_element: Option<PseudoElement>,
     ) -> bool {
         use crate::animation::{AnimationSetKey, AnimationState};
